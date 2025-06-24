16e36e26c5a31a2fab1eb25c827b67165f53eb15
ringo-117 <168697072+ringo-117@users.noreply.github.com>
2025-06-24 11:04:23 +0900

    update-01　TOPページ追加

--- js/main.js
+++ js/main.js
@@ -4,10 +4,14 @@
 {
 	const btn = document.querySelector('.btn');
 	const container = document.querySelector('.container');
+	const header = document.querySelector('.header');
+	const logo = document.querySelector('.logo-box');
 
 	btn.addEventListener('click', () => {
 		btn.classList.toggle('active');
 		container.classList.toggle('active');
+		header.classList.toggle('active');
+		logo.classList.toggle('active');
 
 		// ボディのスクロール制御
 		const bodyStyle = document.body.style;
@@ -26,3 +30,32 @@ $('#nav_list a[href]').on('click', function() {
     $('.btn').trigger('click');
 });
 
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 1.2,
+  centeredSlides: true,
+  spaceBetween: 10,
+  grabCursor: true,
+  // 自動再生の設定
+  autoplay: {
+    delay: 3000, // 3秒ごとに切り替え
+    disableOnInteraction: false // ユーザー操作後も止まらない
+  },
+
+  // ループさせる（無限ループ）
+  loop: true,
+
+  // スライドの切り替えアニメーション（デフォルトは slide）
+  effect: 'slide',
+
+  // アニメーション速度（ミリ秒）
+  speed: 800,
+  // ページネーションが必要なら追加
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  // ナビボタンが必要なら追加
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
